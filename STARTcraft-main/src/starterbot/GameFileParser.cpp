73488659c7b0a2326c5bedc1df0e961690c8c781
PatrickTourniaire <patrick@tourniaire.net>
2024-02-21 14:47:06 +0100

    feat: unit name to unitype enum mapping

--- STARTcraft-main/src/starterbot/GameFileParser.cpp
+++ STARTcraft-main/src/starterbot/GameFileParser.cpp
@@ -6,6 +6,16 @@ namespace fs = std::filesystem;
 
 using json = nlohmann::json;
 
+GameFileParser::GameFileParser() {
+    // Add conversion table entries for the unit types
+    unit_type_map["pylon"] = BWAPI::UnitTypes::Protoss_Pylon;
+    unit_type_map["probe"] = BWAPI::UnitTypes::Protoss_Probe;
+    unit_type_map["gateway"] = BWAPI::UnitTypes::Protoss_Gateway;
+    unit_type_map["assimilator"] = BWAPI::UnitTypes::Protoss_Assimilator;
+
+    cout << unit_type_map.size() << endl;
+};
+
 int GameFileParser::parse_game_file(string const path)
 {
     std::ifstream file(path);
@@ -28,11 +38,8 @@ int GameFileParser::parse_game_file(string const path)
 
 void GameFileParser::print_build_order()
 {
-    fs::path currentPath = fs::current_path();
-    std::cout << "Current absolute path: " << currentPath << std::endl;
-
     for (const auto& building : buildorder) {
-        std::cout << "Name: " << building.getName() << std::endl;
+        std::cout << "Unit Type: " << building.getName() << std::endl;
         std::cout << "Min Production Level: " << building.getMinProductionLevel() << std::endl;
         std::cout << "Max Production Level: " << building.getMaxProductionLevel() << std::endl;
         std::cout << std::endl;
@@ -48,7 +55,7 @@ int GameFileParser::parse_build_order()
 
     std::vector<BuildingRecipe> buildings;
     for (const auto& building : json_file["build_order"]) {
-        std::string name = building["name"];
+        BWAPI::UnitType name = unit_type_map[building["name"]];
         int minProductionLevel = building["minProductionLevel"];
         int maxProductionLevel = building["maxProductionLevel"];
         buildings.emplace_back(name, minProductionLevel, maxProductionLevel);
--- STARTcraft-main/src/starterbot/GameFileParser.hpp
+++ STARTcraft-main/src/starterbot/GameFileParser.hpp
@@ -3,6 +3,9 @@
 #include "json.hpp"
 #include <vector>
 #include <string>
+#include "Tools.h"
+#include "Data.h"
+#include <BWAPI/Type.h>
 
 using namespace std;
 
@@ -11,36 +14,40 @@ using namespace std;
 #define GAME_FILE_PARSER_H
 
 /*
-=================================
-== DATACLASSES USED IN PARSING ==
-=================================
+== DATACLASSES USED IN PARSING
+==============================
 */
 
 class BuildingRecipe {
 public:
     // Constructor
-    BuildingRecipe(const std::string& name, int minProductionLevel, int maxProductionLevel)
+    BuildingRecipe(const BWAPI::UnitType& name, int minProductionLevel, int maxProductionLevel)
         : name(name), minProductionLevel(minProductionLevel), maxProductionLevel(maxProductionLevel) {}
 
     // Accessors
-    std::string getName() const { return name; }
+    BWAPI::UnitType getName() const { return name; }
     int getMinProductionLevel() const { return minProductionLevel; }
     int getMaxProductionLevel() const { return maxProductionLevel; }
 
 private:
-    std::string name;
+    BWAPI::UnitType name;
     int minProductionLevel;
     int maxProductionLevel;
 };
 
 
-
+/*
+== Parses the JSON file into appropriate datatypes
+==================================================
+*/
 
 class GameFileParser
 {
 public:
     std::vector<BuildingRecipe> buildorder;
     nlohmann::json json_file;
+
+    GameFileParser();
 
     int parse_game_file(string const path);
 
@@ -49,9 +56,10 @@ public:
 
 private:
     bool json_loaded;
+    std::unordered_map<std::string, BWAPI::UnitType> unit_type_map;
+
 
     int parse_build_order();
-
 };
 
-#endif // HEADER_NAME_H
+#endif // GAME_FILE_PARSER_H
